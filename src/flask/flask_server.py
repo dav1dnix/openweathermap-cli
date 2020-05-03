96bcd9c64d95862220535932376bfa89c324a816
dps910 <dps141618@gmail.com>
2020-05-03 23:57:38 +0100

    made a start on flask

--- src/flask/flask_server.py
+++ src/flask/flask_server.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Instance of Flask class
+# Instance of class Flask
+# needed so Flask knows where to look for static files etc
 app = Flask(__name__)
 
 @app.route("/")
-def hi():
-    return "hello"
+def root():
+    message = "Hello Flask!"
+    return render_template("index.html", message=message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
