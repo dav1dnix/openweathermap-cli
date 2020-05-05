b2a92b758621250cdb12e2ec4cbe081a14e0b483
David S <david@debian.Home>
2020-05-05 18:05:22 +0100

    bad python code

--- src/flask/flask_server.py
+++ src/flask/flask_server.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template
 
+import sys
+sys.path.append("../")
+
+from index import weather 
+
+w = weather("london")
+
+class Weather:
+    def __init__(self, location, weather, temp, country):
+        self.location = location
+        self.weather = weather
+        self.temp = temp
+        self.country = country
+
+# Data
+location = w["name"]
+weather = w["weather"][0]["description"]
+country = w["sys"]["country"]
+temp = w["main"]["temp"]
+tempFeelsLike = w["main"]["feels_like"]
+print(temp)
+
 # Instance of class Flask
 # needed so Flask knows where to look for static files etc
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    message = "Hello Flask!"
-    return render_template("index.html", message=message)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/index.py
+++ src/index.py
@@ -3,16 +3,15 @@ import chalk
 import json
 import argparse
 
-with open("./config.json") as json_file:
+with open("../config.json") as json_file:
 	x = json.load(json_file)
 
-parser = argparse.ArgumentParser(description="Get information about the weather")
+#parser = argparse.ArgumentParser(description="Get information about the weather")
 
 # --location argument
-parser.add_argument("--location", "-l", type=str, help="location", metavar="", required=True)
+#parser.add_argument("--location", "-l", type=str, help="location", metavar="", required=True)
 
-
-arguments = parser.parse_args()
+#arguments = parser.parse_args()
 def weather(location):
 	url = "https://api.openweathermap.org/data/2.5/weather?q="
 	api_key = f"&appid={x['api_key']}"
@@ -23,4 +22,4 @@ if not x["api_key"]:
 	print(chalk.red("Please add an API key to config.json"))
 
 if __name__ == "__main__":
-	print(weather(arguments.location))
+        print(weather)
